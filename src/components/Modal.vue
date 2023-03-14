a016e58099e738d31796194c043bf56ab13a4af3
Elvan Hidayat <elvanhd@gmail.com>
2023-03-14 21:11:43 +0700

    define props

--- src/components/Modal.vue
+++ src/components/Modal.vue
@@ -1,20 +1,24 @@
 <template>
   <teleport to=".modals-container">
     <div class="modal">
-      <h1><slot name="title" /></h1>
+      <h1>{{ title }}</h1>
       <slot />
-      <!-- <pre>{{ $slots.title() }}</pre> -->
       <button>Hide modal</button>
     </div>
   </teleport>
 </template>
 
 <script setup>
-import { useSlots } from 'vue';
+/*
+  props
+*/
 
-const slots = useSlots();
-
-// console.log(slots.title());
+const props = defineProps({
+  title: {
+    type: String,
+    default: 'No title specified',
+  },
+});
 </script>
 
 <style>
