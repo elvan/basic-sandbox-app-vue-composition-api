7722c88e4f2111cf51bb80bd3729c0338622982d
Elvan Hidayat <elvanhd@gmail.com>
2023-03-14 19:27:15 +0700

    script setup

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -8,27 +8,17 @@
   </div>
 </template>
 
-<script>
+<script setup>
 import { ref } from 'vue';
 
-export default {
-  setup() {
-    const counter = ref(0);
+const counter = ref(0);
 
-    const increaseCounter = () => {
-      counter.value++;
-    };
+const increaseCounter = () => {
+  counter.value++;
+};
 
-    const decreaseCounter = () => {
-      counter.value--;
-    };
-
-    return {
-      counter,
-      increaseCounter,
-      decreaseCounter,
-    };
-  },
+const decreaseCounter = () => {
+  counter.value--;
 };
 </script>
 
