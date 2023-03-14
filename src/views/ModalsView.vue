abca82e53f90f3036ff81b22e7d3b2934ddfed57
Elvan Hidayat <elvanhd@gmail.com>
2023-03-14 21:02:02 +0700

    child components

--- src/views/ModalsView.vue
+++ src/views/ModalsView.vue
@@ -2,17 +2,7 @@
   <div class="modals">
     <h1>Modals</h1>
     <button @click="showModal = true">Show modal</button>
-    <teleport to=".modals-container">
-      <div v-if="showModal" class="modal">
-        <h1>This is a modal</h1>
-        <p>
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. Beatae ipsa
-          laboriosam vero natus ut rerum quaerat, saepe praesentium tempore et
-          hic velit odio nemo minus labore quam ullam quod architecto?
-        </p>
-        <button @click="showModal = false">Hide modal</button>
-      </div>
-    </teleport>
+    <Modal v-if="showModal"></Modal>
   </div>
 </template>
 
@@ -21,6 +11,7 @@
   imports
 */
 
+import Modal from '@/components/Modal.vue';
 import { ref } from 'vue';
 
 /*
@@ -29,16 +20,3 @@ import { ref } from 'vue';
 
 const showModal = ref(false);
 </script>
-
-<style>
-.modal {
-  background: beige;
-  padding: 10px;
-  position: absolute;
-  left: 0;
-  top: 0;
-  width: 100%;
-  height: 100%;
-  z-index: 1;
-}
-</style>
